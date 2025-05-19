a5c54a19841cf461cf8bb0db35df44f857724b6d
NuclearMissile <torrific55@gmail.com>
2025-05-19 11:13:09 +0900

    update

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,6 @@ import {useEffect, useState} from 'react';
 import {toast, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './style.css';
-import {CgNotes} from "react-icons/cg";
 import {FaCheck} from "react-icons/fa";
 import {FaListCheck} from "react-icons/fa6";
 
@@ -57,8 +56,7 @@ export default function TodoList() {
 
     const handleTaskCheckboxChange = (taskId) => {
         setTasks((prevTasks) => prevTasks.map((task) => task.id === taskId ? {
-            ...task,
-            completed: !task.completed
+            ...task, completed: !task.completed
         } : task));
     };
 
@@ -99,8 +97,7 @@ export default function TodoList() {
             });
             const updatedTaskData = await response.json();
             setTasks((prevTasks) => prevTasks.map((task) => task.id === editTaskId ? {
-                ...task,
-                title: updatedTaskData.title
+                ...task, title: updatedTaskData.title
             } : task));
             setInputValue('');
             setEditTaskId(null);
@@ -111,9 +108,7 @@ export default function TodoList() {
         }
     };
 
-    const handleCompleteAll = () =>
-        setTasks((tasks) => tasks.map((task) => ({...task, completed: true})));
-
+    const handleCompleteAll = () => setTasks((tasks) => tasks.map((task) => ({...task, completed: true})));
 
     const handleFilterChange = (filterType) => setFilter(filterType);
 
@@ -128,85 +123,83 @@ export default function TodoList() {
         return true;
     });
 
-    return (
-        <div className="container">
-            <ToastContainer/>
-            <div className="todo-app">
-                <h2>
-                    <CgNotes/> Todo List
-                </h2>
-                <div className="row">
-                    <FaListCheck/>
-                    <input
-                        type="text"
-                        className="add-task"
-                        id="add"
-                        placeholder="Add your todo"
-                        autoFocus
-                        value={inputValue}
-                        onChange={handleInputChange}
-                    />
-                    <button id="btn" onClick={editTaskId ? handleUpdateTask : handleAddTask}>
-                        {editTaskId ? 'Update' : 'Add'}
-                    </button>
-                </div>
+    return (<div className="container">
+        <ToastContainer/>
+        <div className="todo-app">
+            <h2>
+                <a href='https://github.com/NuclearMissile/react-todo-list'>Todo List</a>
+            </h2>
+            <div className="row">
+                <FaListCheck/>
+                <input
+                    type="text"
+                    className="add-task"
+                    id="add"
+                    placeholder="Add your todo"
+                    autoFocus
+                    value={inputValue}
+                    onChange={handleInputChange}
+                />
+                <button id="btn" onClick={editTaskId ? handleUpdateTask : handleAddTask}>
+                    {editTaskId ? 'Update' : 'Add'}
+                </button>
+            </div>
 
-                <div className="mid">
-                    <FaCheck/>
-                    <p id="complete-all" onClick={handleCompleteAll} style={{marginLeft: 10}}>Complete all</p>
-                </div>
+            <div className="mid">
+                <FaCheck/>
+                <p id="complete-all" onClick={handleCompleteAll} style={{marginLeft: 10}}>Complete all</p>
+            </div>
 
-                <ul id="list">
-                    {filteredTasks.map((task) => (
-                        <li key={task.id} className={editTaskId === task.id ? 'edit-mode' : ''}>
-                            <input
-                                type="checkbox"
-                                id={`task-${task.id}`}
+            <ul id="list">
+                {filteredTasks.map((task) => (
+                    <li key={task.id} className={editTaskId === task.id ? 'edit-mode' : ''}>
+                        <input
+                            type="checkbox"
+                            id={`task-${task.id}`}
+                            data-id={task.id}
+                            className="custom-checkbox"
+                            checked={task.completed}
+                            onChange={() => handleTaskCheckboxChange(task.id)}
+                        />
+                        <label htmlFor={`task-${task.id}`}>{task.title}</label>
+                        <div>
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png"
+                                className="edit"
                                 data-id={task.id}
-                                className="custom-checkbox"
-                                checked={task.completed}
-                                onChange={() => handleTaskCheckboxChange(task.id)}
+                                onClick={() => handleEditTask(task.id)}
+                                alt='edit'
                             />
-                            <label htmlFor={`task-${task.id}`}>{task.title}</label>
-                            <div>
-                                <img
-                                    src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png"
-                                    className="edit"
-                                    data-id={task.id}
-                                    onClick={() => handleEditTask(task.id)}
-                                    alt='edit'
-                                />
-                                <img
-                                    src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png"
-                                    className="delete"
-                                    data-id={task.id}
-                                    onClick={() => handleDeleteTask(task.id)}
-                                    alt='delete'
-                                />
-                            </div>
-                        </li>))}
-                </ul>
-
-                <div className="filters">
-                    <div className="dropdown">
-                        <button className="dropbtn">Filter</button>
-                        <div className="dropdown-content">
-                            <a className={filter === 'all' ? 'filter-selected' : ''}
-                               href="#" id="all" onClick={() => handleFilterChange('all')}>
-                                All
-                            </a>
-                            <a className={filter === 'uncompleted' ? 'filter-selected' : ''}
-                               href="#" id="rem" onClick={() => handleFilterChange('uncompleted')}>
-                                Uncompleted
-                            </a>
-                            <a className={filter === 'completed' ? 'filter-selected' : ''} href="#"
-                               id="com" onClick={() => handleFilterChange('completed')}>
-                                Completed
-                            </a>
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png"
+                                className="delete"
+                                data-id={task.id}
+                                onClick={() => handleDeleteTask(task.id)}
+                                alt='delete'
+                            />
                         </div>
+                    </li>))}
+            </ul>
+
+            <div className="filters">
+                <div className="dropdown">
+                    <button className="dropbtn">Filter</button>
+                    <div className="dropdown-content">
+                        <a className={filter === 'all' ? 'filter-selected' : ''}
+                           href="#" id="all" onClick={() => handleFilterChange('all')}>
+                            All
+                        </a>
+                        <a className={filter === 'uncompleted' ? 'filter-selected' : ''}
+                           href="#" id="rem" onClick={() => handleFilterChange('uncompleted')}>
+                            Uncompleted
+                        </a>
+                        <a className={filter === 'completed' ? 'filter-selected' : ''} href="#"
+                           id="com" onClick={() => handleFilterChange('completed')}>
+                            Completed
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    </div>);
 };
